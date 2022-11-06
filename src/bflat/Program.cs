bflattened/bflat

commit df6c2730530dad93e54122ae32d942918c55aef3

Get rid of System.CommandLine garbage (#44)
Like I wrote in #32 - "Already regretting the switch to System.CommandLine :("

Apparently, in default configuration System.CommandLine will write to /tmp as part of argument parsing. WTF.

src/bflat/Program.cs
@@ -16,7 +16,9 @@
 
 using System;
 using System.CommandLine;
+using System.CommandLine.Builder;
 using System.CommandLine.Help;
+using System.CommandLine.Parsing;
 using System.Reflection;
 
 class Program
@@ -62,12 +64,17 @@ class Program
             }
         });
 
+        Parser parser = new CommandLineBuilder(root)
+                .UseVersionOption("-v")
+                .UseParseErrorReporting()
+                .Build();
+
 #if DEBUG
-        return root.Invoke(args);
+        return parser.Invoke(args);
 #else
         try
         {
-            return root.Invoke(args);
+            return parser.Invoke(args);
         }
         catch (Exception e)
         {

commit 70ad110d832547fc78b3b426004b9852a8ee2b4f

Fix `--help`
Broke this in #44.

src/bflat/Program.cs
@@ -67,6 +67,7 @@ class Program
         Parser parser = new CommandLineBuilder(root)
                 .UseVersionOption("-v")
                 .UseParseErrorReporting()
+                .UseHelp()
                 .Build();
 
 #if DEBUG